85d92e693cc23c20f867546d2c456b39f6cf4a68
Brent Van Minnen <bjvanminnen@gmail.com>
2016-01-28 00:02:12 -0800

    basic routing working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
 
+import { Router, Route, Link } from 'react-router'
+
 import Chart1 from './Chart1';
 import Chart2 from './Chart2';
 
+const Page = (props) => {
+  if (props.children) {
+    return (
+      <div>
+        <div><Link to="/">Back</Link></div>
+        {props.children}
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div>BaseApp</div>
+      <div><Link to="/chart1">Chart1</Link></div>
+      <div><Link to="/chart2">Chart2</Link></div>
+    </div>
+  );
+};
+
 const App = () => (
-  <div>
-    <Chart1/>
-    <Chart2/>
-  </div>
+  <Router>
+    <Route path="/" component={Page}>
+      <Route path="chart1" component={Chart1}/>
+      <Route path="chart2" component={Chart2}/>
+    </Route>
+  </Router>
 );
 export default App;
