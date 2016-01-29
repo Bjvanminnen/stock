a64dde12ecbeb82cc1996a71ac7b924eb4138fcd
Brent Van Minnen <bjvanminnen@gmail.com>
2016-01-29 10:33:27 -0800

    Slightly refactored routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,38 @@
 import React from 'react';
 
-import { Router, Route, Link } from 'react-router'
+import { Router, Route, Link, IndexRoute, IndexLink } from 'react-router'
 
 import Chart1 from './Chart1';
 import Chart2 from './Chart2';
 
-const Page = (props) => {
-  if (props.children) {
-    return (
-      <div>
-        <div><Link to="/">Back</Link></div>
-        {props.children}
-      </div>
-    );
-  }
+const LinkToHome = () => (
+  <div><IndexLink to="/">Back</IndexLink></div>
+);
 
+const Page = (props) => {
+  console.log(props);
+  console.log(props.location.pathname);
   return (
     <div>
-      <div>BaseApp</div>
-      <div><Link to="/chart1">Chart1</Link></div>
-      <div><Link to="/chart2">Chart2</Link></div>
+      {props.location.pathname !== "/" ? <LinkToHome/> : undefined}
+      {props.children}
     </div>
   );
+
 };
+
+const Index = () => (
+  <div>
+    <div>BaseApp</div>
+    <div><Link to="/chart1">Chart1</Link></div>
+    <div><Link to="/chart2">Chart2</Link></div>
+  </div>
+);
 
 const App = () => (
   <Router>
     <Route path="/" component={Page}>
+      <IndexRoute component={Index}/>
       <Route path="chart1" component={Chart1}/>
       <Route path="chart2" component={Chart2}/>
     </Route>
