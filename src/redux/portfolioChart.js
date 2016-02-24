1d5d1b8fa1e30ba6d9e032cce20bfb244a0345d2
Brent Van Minnen <bjvanminnen@gmail.com>
2016-02-23 23:11:24 -0800

    minimal progress to portfolio chart reducer

--- src/redux/portfolioChart.js
+++ src/redux/portfolioChart.js
@@ -1,16 +1,29 @@
 import { CHANGE_ACTIVE_PORTFOLIO } from './actions';
 
 const initialState = {
-  current: undefined
+  current: undefined,
+  data: undefined
 };
 
-export default function portfolioChart(state = initialState, action) {
+export default function portfolioChart(state = initialState, action, dataResponses) {
+  let newState = state;
   if (action.type === CHANGE_ACTIVE_PORTFOLIO) {
-    return {
-      ...state,
-      current: action.id
-    };
+    newState = setCurrentChart(state, id);
   }
 
+  // TODO - to make progress, we need a proper data store
+  newState = setChartData(newState, dataResponses);
+
+  return newState;
+}
+
+function setCurrentChart(state, id) {
+  return {
+    ...state,
+    current: id
+  };
+}
+
+function setChartData(state, stockData) {
   return state;
 }
